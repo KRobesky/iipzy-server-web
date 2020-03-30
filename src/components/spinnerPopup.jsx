f3cfc5e6f2b808c2afba6be676864a90799c4233
ken Robesky <ken@robesky.org>
2020-03-30 11:50:12 -0700

    spinner on User form

--- src/components/spinnerPopup.jsx
+++ src/components/spinnerPopup.jsx
@@ -26,19 +26,19 @@ class SpinnerPopup extends React.Component {
     return (
       <div>
         <Navigator />
-        {/*         <div className="popup_spinner"> */}
-        <div className="popup_spinner_inner" alignItems="center">
-          {/*             <div style={{ marginLeft: 20, textAlign: "left" }}>
+        <div className="popup_spinner">
+          <div className="popup_spinner_inner" alignItems="center">
+            {/*             <div style={{ marginLeft: 20, textAlign: "left" }}>
               <p style={{ fontSize: "140%" }}>{this.props.title}</p>
             </div> */}
-          {/* <div style={{ marginLeft: "-60px" }}> */}
-          <div>
-            <Spinner animation="border" role="status">
-              <span className="sr-only">Loading...</span>
-            </Spinner>
+            {/* <div style={{ marginLeft: "-60px" }}> */}
+            <div>
+              <Spinner animation="border" role="status">
+                <span className="sr-only">Loading...</span>
+              </Spinner>
+            </div>
           </div>
         </div>
-        {/*         </div> */}
       </div>
     );
   }
