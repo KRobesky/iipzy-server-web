41207be98b0541e73ff1e371008499079c98e418
ken Robesky <ken@robesky.org>
2023-04-25 04:15:18 -0700

    degugging

---
+++ build/precache-manifest.d145d4455a1f17c6732b8b4e93dcbd85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a62e3a0b75b9fe893d668936bcb0d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31a97e6edb59821c8d28",
+    "url": "/static/css/2.fffcb0a3.chunk.css"
+  },
+  {
+    "revision": "048d76929138d363bab3",
+    "url": "/static/css/main.ab299cab.chunk.css"
+  },
+  {
+    "revision": "31a97e6edb59821c8d28",
+    "url": "/static/js/2.34f67fb7.chunk.js"
+  },
+  {
+    "revision": "32da10e55b95b7b455b39c07282832ac",
+    "url": "/static/js/2.34f67fb7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "048d76929138d363bab3",
+    "url": "/static/js/main.ee02d1a7.chunk.js"
+  },
+  {
+    "revision": "8b29a95cf6e6eb1101a3",
+    "url": "/static/js/runtime-main.956bc44c.js"
+  }
+]);
